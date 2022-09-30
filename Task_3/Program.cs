52a601c409a7205e26a52e2d5ddfee5343795d78
El_Dan <lilit-li@ya.ru>
2022-09-30 16:18:19 +0400

    Исправление решения задачи 3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -10,30 +10,51 @@ Console.WriteLine("Введите количество столбцов: ");
 int cols = Convert.ToInt32(Console.ReadLine());
 int[,] Array = new int[rows, cols];
 if ((rows * cols) > 50)
-{
-    Console.WriteLine("Количество элементов больше 50");
-}
-else
-    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols; j++)
-        {
-            Array[i, j] = new Random().Next(1, 100);
-            
-            
-            //for  (int k = 1; k < rows; k++)
-           // {
-                for (int l = 1; l < cols; l++)
-                {
-                    if (Array[i, j] == Array[i, l])
-                            Array[i, l] = new Random().Next(1, 100);
-                }
-            //}
-        Console.Write($"{Array[i, j]} "); //вывод значений массива для примера
-        }
-
-    Console.WriteLine();    
-
+        Console.WriteLine("Количество элементов больше 50");
     }
+    else
+        {
+            CreateArray(Array);
+            for (int i = 0; i < Array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Array.GetLength(1); j++)
+                        {
+                            Console.Write(Array[i, j] +" ");
+                        }
+                Console.WriteLine();
+                }
+        }
    
-
+void CreateArray(int[,] Array)
+{
+  int[] temp = new int[Array.GetLength(0) * Array.GetLength(1)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 99);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 99);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int m = 0; m < Array.GetLength(0); m++)
+  {
+    for (int n = 0; n < Array.GetLength(1); n++)
+    {
+        Array[m, n] = temp[count];
+        count++;
+    }
+  }
+}
